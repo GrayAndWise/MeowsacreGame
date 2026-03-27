f203d843b72623fa2c97c819e222c9795bcaefb6
GrayAndWise <zilaitisstuff@gmail.com>
2026-03-28 01:16:25 +0200

    just some simple refactorings
    aaa

--- cameraview.gd
+++ cameraview.gd
@@ -1,6 +1,7 @@
 extends AnimatedSprite2D
 
-var targetFrame : int = 0
+var targetFrame: int = 0
+var currentFrame: int = 0  # Tracks which camera is active
 
 func _ready() -> void:
 	$cam1btn.mouse_entered.connect(func(): switchRoom(1))
@@ -8,6 +9,15 @@ func _ready() -> void:
 	$AnimatedSprite2D.animation_finished.connect(func(): $roomSelect.frame = targetFrame)
 
 func switchRoom(roomFrame: int) -> void:
+	# Only trigger if switching to a new camera
+	if roomFrame == currentFrame:
+		return
+	
+	# Step 1: Update camera frame immediately
 	targetFrame = roomFrame
+	currentFrame = roomFrame
+	$roomSelect.frame = targetFrame  # instantly switch the visible camera
+
+	# Step 2: Play glitch animation & sound
 	$AnimatedSprite2D.play()
 	$AudioStreamPlayer2D.play()
--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,50 +1,68 @@
 extends Label
 
-var camerasOpen : bool = false
+var cameras_open: bool = false
+
+# --- Cache all relevant nodes ---
+@onready var camera = $"../../camera"
+@onready var static_sprite = camera.get_node("Static")
+@onready var anim_sprite = camera.get_node("AnimatedSprite2D")
+@onready var audio_player = camera.get_node("AudioStreamPlayer2D")
+@onready var ui_nodes = [
+	camera.get_node("roomSelect"),
+	camera.get_node("ColorRect"),
+	camera.get_node("camMap"),
+	camera.get_node("cam1btn"),
+	camera.get_node("cam2btn")
+]
 
 func _ready() -> void:
-	mouse_entered.connect(mouseEntered)
-	mouse_exited.connect(mouseExited)
-	$"../../camera/roomSelect".visible=false
-
-
-func mouseEntered() -> void:
-	pass
-	# show some hover state, cursor change etc
-
-func mouseExited() -> void:
-	pass
-	# revert hover state
-
-func _gui_input(event : InputEvent) -> void:
-	if event.is_action_pressed("gui_click"):
-		camerasOpen = !camerasOpen
-		if camerasOpen:
-			openCameras()
-		else:
-			closeCameras()
-
-func openCameras() -> void:
-	$"../../camera".play()
-	await $"../../camera".animation_finished
-	$"../../camera/Static".visible = true
-	$"../../camera/Static".play()
-	$"../../camera/AnimatedSprite2D".play()
-	$"../../camera/AudioStreamPlayer2D".play()
-	$"../../camera/roomSelect".visible = true
-	$"../../camera/ColorRect".visible = true
-	$"../../camera/camMap".visible = true
-	$"../../camera/cam1btn".visible = true
-	$"../../camera/cam2btn".visible = true
-
-func closeCameras() -> void:
-	$"../../camera/AnimatedSprite2D".play()
-	$"../../camera/Static".visible = false
-	$"../../camera/Static".stop()
-	$"../../camera/ColorRect".visible = false
-	$"../../camera/camMap".visible = false
-	$"../../camera/cam1btn".visible = false
-	$"../../camera/cam2btn".visible = false
-	$"../../camera/roomSelect".visible = false
-	$"../../camera".play_backwards()
+	# connect signals properly
+	self.mouse_entered.connect(_on_mouse_entered)
+	self.mouse_exited.connect(_on_mouse_exited)
 	
+	set_camera_ui_visible(false)
+
+# --- Hover callbacks ---
+func _on_mouse_entered() -> void:
+	# optional: cursor change, highlight, etc
+	pass
+
+func _on_mouse_exited() -> void:
+	# optional: revert hover effect
+	pass
+
+# --- Input handler ---
+func _gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("gui_click"):
+		toggle_cameras()
+
+# --- Toggle cameras ---
+func toggle_cameras() -> void:
+	cameras_open = !cameras_open
+	if cameras_open:
+		open_cameras()
+	else:
+		close_cameras()
+
+# --- Unified UI toggle ---
+func set_camera_ui_visible(state: bool) -> void:
+	for node in ui_nodes:
+		node.visible = state
+
+# --- Open cameras with glitch/animation/audio ---
+func open_cameras() -> void:
+	camera.play()
+	await camera.animation_finished  # wait for glitch animation to finish
+	static_sprite.visible = true
+	static_sprite.play()
+	anim_sprite.play()
+	audio_player.play()
+	set_camera_ui_visible(true)
+
+# --- Close cameras ---
+func close_cameras() -> void:
+	anim_sprite.play()
+	static_sprite.visible = false
+	static_sprite.stop()
+	set_camera_ui_visible(false)
+	camera.play_backwards()
