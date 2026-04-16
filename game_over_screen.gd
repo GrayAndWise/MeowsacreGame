f3ce53519116a282808fc836a4141d784194408f
AFloofee <ainmat@ktu.lt>
2026-04-16 03:15:37 +0300

    Updade 5.6
    Added game over

---
+++ game_over_screen.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_tryagain_pressed() -> void:
+	get_tree().change_scene_to_file("res://night.tscn")
+
+
+func _on_mainmenu_pressed() -> void:
+	get_tree().change_scene_to_file("res://title.tscn")
