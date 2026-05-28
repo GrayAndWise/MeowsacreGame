5fd2ce60def117463c718982513a6af3d710e93f
GrayAndWise <zilaitisstuff@gmail.com>
2026-05-28 10:50:03 +0300

    added win art
    Simple art for win scene

--- scripts/win.gd
+++ scripts/win.gd
@@ -3,7 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	await get_tree().create_timer(5).timeout
+	$background/TextureRect.visible = true
+	await get_tree().create_timer(10).timeout
 	get_tree().change_scene_to_file("res://title.tscn")
 
 
